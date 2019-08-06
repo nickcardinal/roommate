6b156d82ed26894ad69a609297aa8f67eb351460
Inditorias <46539483+jstut001@users.noreply.github.com>
2019-08-06 07:39:59 +0100

    Added vs project files created cofig.js from firebase config

---
+++ Roomate-app/README.md
@@ -0,0 +1,3 @@
+﻿# Roomate-app
+
+
---
+++ Roomate-app/config.js
@@ -0,0 +1,9 @@
+﻿const firebaseConfig = {
+    apiKey: "AIzaSyCmyzsBoBimz_LSIKGBVPqYb9-Ngq4aW38",
+    authDomain: "roommate-a0823.firebaseapp.com",
+    databaseURL: "https://roommate-a0823.firebaseio.com",
+    projectId: "roommate-a0823",
+    storageBucket: "roommate-a0823.appspot.com",
+    messagingSenderId: "941174280385",
+    appId: "1:941174280385:web:0ee7547beb6089b7"
+};
