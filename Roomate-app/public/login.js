502dfef716e4e0301473e32f4b28488dfaed959d
jstut001 <46539483+jstut001@users.noreply.github.com>
2019-08-07 09:11:10 +0100

    Google auth is functioning

--- Roomate-app/public/login.js
+++ Roomate-app/public/login.js
@@ -1,68 +1,42 @@
 ﻿function initialize() {
     firebase.initializeApp(firebaseConfig);
 }
-//var provider = new firebase.auth.GoogleAuthProvider();
 
-
-
-function loginWithPopup() {
-    provider = new firebase.default.auth.GoogleAuthProvider();
-    firebase.auth.signInWithPopup(provider).then(function (result) {
-        // This gives you a Google Access Token. You can use it to access the Google API.
+function login() {
+    var provider = new firebase.auth.GoogleAuthProvider();
+    provider.addScope('https://www.googleapis.com/auth/userinfo.email');
+    firebase.auth().signInWithPopup(provider).then(function (result) {
         var token = result.credential.accessToken;
-        // The signed-in user info.
         var user = result.user;
-        // ...
     }).catch(function (error) {
-        // Handle Errors here.
         var errorCode = error.code;
         var errorMessage = error.message;
-        // The email of the user's account used.
         var email = error.email;
-        // The firebase.auth.AuthCredential type that was used.
         var credential = error.credential;
-        // ...
     });
-    onSignIn(user);
-
 }
 
-function loginWithRedirect() {
-    firebase.auth.getRedirectResult().then(function (result) {
+function redirLogin() {
+    var provider = new firebase.auth.GoogleAuthProvider();
+    provider.addScope('https://www.googleapis.com/auth/userinfo.email');
+    firebase.auth().signInWithRedirect(provider);
+    firebase.auth().getRedirectResult().then(function (result) {
         if (result.credential) {
-            // This gives you a Google Access Token.
             var token = result.credential.accessToken;
         }
         var user = result.user;
+    }).catch(function (error) {
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        var email = error.email;
+        var credential = error.credential;
     });
-
-    //var provider = new firebase.auth.GoogleAuthProvider();
-    //firebase.auth().signInWithRedirect(provider);
-    //firebase.auth().getRedirectResult().then(function (result) {
-    //    if (result.credential) {
-    //        // This gives you a Google Access Token. You can use it to access the Google API.
-    //        var token = result.credential.accessToken;
-    //        // ...
-    //    }
-    //    // The signed-in user info.
-    //    var user = result.user;
-    //}).catch(function (error) {
-    //    // Handle Errors here.
-    //    var errorCode = error.code;
-    //    var errorMessage = error.message;
-    //    // The email of the user's account used.
-    //    var email = error.email;
-    //    // The firebase.auth.AuthCredential type that was used.
-    //    var credential = error.credential;
-    //    // ...
-    //});
 }
 
-function onSignIn(googleUser) {
-    var profile = googleUser.getBasicProfile();
-    console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
-    console.log('Name: ' + profile.getName());
-    console.log('Image URL: ' + profile.getImageUrl());
-    console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
-}
+function logout() {
+    firebase.auth().signOut().then(function () {
 
+    }).catch(function (error) {
+
+    });
+}
