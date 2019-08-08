7ada04cc1f216bbef8ee4fe1f3873ad3a2eafc74
jstut001 <46539483+jstut001@users.noreply.github.com>
2019-08-08 08:44:42 +0100

    removed redirect login function added console log for sign in error

--- Roomate-app/public/login.js
+++ Roomate-app/public/login.js
@@ -7,29 +7,14 @@ function login() {
     provider.addScope('https://www.googleapis.com/auth/userinfo.email');
     firebase.auth().signInWithPopup(provider).then(function (result) {
         var token = result.credential.accessToken;
+        console.log(token);
         var user = result.user;
     }).catch(function (error) {
         var errorCode = error.code;
         var errorMessage = error.message;
         var email = error.email;
         var credential = error.credential;
-    });
-}
-
-function redirLogin() {
-    var provider = new firebase.auth.GoogleAuthProvider();
-    provider.addScope('https://www.googleapis.com/auth/userinfo.email');
-    firebase.auth().signInWithRedirect(provider);
-    firebase.auth().getRedirectResult().then(function (result) {
-        if (result.credential) {
-            var token = result.credential.accessToken;
-        }
-        var user = result.user;
-    }).catch(function (error) {
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        var email = error.email;
-        var credential = error.credential;
+        console.log("ERROR: " + errorCode + " : " + errorMessage);
     });
 }
 
