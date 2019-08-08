e2e571abf48193be3fbd96c7349e15b3662a59d3
jstut001 <46539483+jstut001@users.noreply.github.com>
2019-08-08 11:00:43 +0100

    Added temp favcon Added variables for user name, email, photo

--- Roomate-app/public/login.js
+++ Roomate-app/public/login.js
@@ -3,17 +3,23 @@
 }
 
 function login() {
+    var token, user, name, email, photoUrl, verified;
     var provider = new firebase.auth.GoogleAuthProvider();
     provider.addScope('https://www.googleapis.com/auth/userinfo.email');
     firebase.auth().signInWithPopup(provider).then(function (result) {
-        var token = result.credential.accessToken;
-        console.log(token);
-        var user = result.user;
+        token = result.credential.accessToken;
+        user = result.user;
+        name = user.displayName;
+        email = user.email;
+        photoUrl = user.photoURL;
+        emailVerified = user.emailVerified;
+        console.log(user.email)
+        window.alert("Welcome, " + name);        
     }).catch(function (error) {
         var errorCode = error.code;
         var errorMessage = error.message;
-        var email = error.email;
-        var credential = error.credential;
+        //var email = error.email;
+        //var credential = error.credential;
         console.log("ERROR: " + errorCode + " : " + errorMessage);
     });
 }
