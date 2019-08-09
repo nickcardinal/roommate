08078461bfef27666d1ec5fe2b216a9f27d406dd
jstut001 <46539483+jstut001@users.noreply.github.com>
2019-08-09 08:34:42 +0100

    Added member variables for space and task

--- Roomate-app/public/Space.js
+++ Roomate-app/public/Space.js
@@ -1,5 +1,8 @@
 ﻿class Space {
     constructor() {
-        
+        this.tasks = new Array();
+        this.name = 'My space';
+        this.taskCategories = new Array();
+        this.members = new array();
     }
 }
--- Roomate-app/public/Task.js
+++ Roomate-app/public/Task.js
@@ -1,3 +1,11 @@
 ﻿class Task {
+    constructor() {
+        this.taskCategories = new Array();
+        this.title = '';
+        this.time = new Date();
+        this.complete = false;
+        this.desc = '';
+        this.assignedMembers = new Array();
 
+    }
 }
