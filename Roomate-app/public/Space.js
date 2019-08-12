d6aa912062a7086c3928a7f5c308d59f10b4402c
meidam <53287473+meidam@users.noreply.github.com>
2019-08-12 08:22:21 -0700

    Added getters/setters to Space.js (#8)

--- Roomate-app/public/Space.js
+++ Roomate-app/public/Space.js
@@ -1,9 +1,49 @@
 ﻿class Space {
     constructor() {
-        this.tasks = [];
         this.name;
-        this.taskCategories = [];
-        this.members = [];
-        this.sid;
+        this.description;
+        this.spaceID;
+        this.mates = [];
+        this.tasks = [];
+    }
+
+    setName(name) {
+      this.name = name;
+    }
+
+    getName() {
+      return this.name;
+    }
+
+    setDescription(description) {
+      this.description = description;
+    }
+
+    getDescription() {
+      return this.description;
+    }
+
+    setSpaceID(spaceID) {
+      this.spaceID = spaceId;
+    }
+
+    getSpaceID() {
+      return this.spaceID;
+    }
+
+    addMate(mate) {
+      this.mates.push(mate);
+    }
+
+    getMates() {
+      return this.mates;
+    }
+
+    addTask(task) {
+      this.tasks.push(task);
+    }
+
+    getTasks() {
+      return this.tasks;
     }
 }
