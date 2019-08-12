36b4eb58ce2fc8ac3aec08a4c317a7c1b63d73cd
meidam <53287473+meidam@users.noreply.github.com>
2019-08-12 05:05:46 -0700

    Removed unnecessary functions in Space.js (#5)

--- Roomate-app/public/Space.js
+++ Roomate-app/public/Space.js
@@ -1,15 +1,9 @@
 ﻿class Space {
-    constructor(spaceId) {
-        this.tasks = new Array();
-        this.name = 'My space';
-        this.taskCategories = new Array();
-        this.members = new array();
-        this.sid = spaceId;
-        //data member initialized
-        const spaceRef = db.collection("LivingGroup").doc(sid);
-        let getDoc = spaceRef.get();
-    }
-    pushToFirestore() {
-
+    constructor() {
+        this.tasks = [];
+        this.name;
+        this.taskCategories = [];
+        this.members = [];
+        this.sid;
     }
 }
