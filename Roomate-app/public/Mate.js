03f3e1ea6b089c1cbfbe03bf3d19c7e558cadd15
atran112 <53277408+atran112@users.noreply.github.com>
2019-08-12 16:20:42 +0100

    Added setter and getter functions for the Mate class (#7)

--- Roomate-app/public/Mate.js
+++ Roomate-app/public/Mate.js
@@ -1,5 +1,6 @@
 
 class Mate {
+    
     constructor() {
         this.googleAuthToken;
         this.user_ID;
@@ -9,4 +10,53 @@ class Mate {
         this.photoUrl;
         //test commit
     }
+    
+    setToken(googleAuthToken) {
+        this.googleAuthToken = googleAuthToken;
+    }
+    
+    getToken() {
+        return this.googleAuthToken;
+    }
+    
+    setID(user_ID) {
+        this.user_ID = user_ID;
+    }
+    
+    getID() {
+        return this.user_ID;
+    }
+    
+    setNickName(nickName) {
+        this.nickName = nickName;
+    }
+    
+    getNickName() {
+        return this.nickName;
+    }
+    
+    setFullName(fullName) {
+        this.fullName = fullName;
+    }
+    
+    getFullName() {
+        return this.fullName;
+    }
+    
+    setEmail(email) {
+        this.email = email;
+    }
+    
+    getEmail() {
+        return this.email;
+    }
+    
+    setPhotoURL(photoURL) {
+        this.photoURL = photoURL;
+    }
+    
+    getPhotoURL() {
+        return this.photoURL;
+    }
+    
 }
