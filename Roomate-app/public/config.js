a8f130b48aab341c9aabb93e90b5dad30944e09d
jstut001 <46539483+jstut001@users.noreply.github.com>
2019-08-13 17:21:39 +0100

    Most bugs squashed
    For whatever reason redirecting to overview fails to update token and time in mates database
    
    login.js :: 75

--- Roomate-app/public/config.js
+++ Roomate-app/public/config.js
@@ -11,13 +11,21 @@ const firebaseConfig = {
     appId: "1:941174280385:web:0ee7547beb6089b7"
 };
 function validate(){
-    initialize();
+    firebase.initializeApp(firebaseConfig);
     database = firebase.firestore();
-    let query = database.collection('Mates').where('usrEmail', '==', sessionStorage.getItem(0)).get().then(snapshot =>{
+    let query = database.collection('Mates').where('athToken', '==', sessionStorage.getItem(0)).get().then(snapshot =>{
         if(snapshot.empty){
             window.location.href = "./index.html";
+            console.log('Invalid Token : ', sessionStorage.getItem(0));
         }else{
-            return;
+            snapshot.forEach(doc => {
+                if(new Date() < doc.data().athExpiration.toDate()){
+                    sessionStorage.setItem(1, doc.id);
+                }else{
+                    window.location.href = "./index.html";
+                }
+                
+            });
         }
     });
   }
--- Roomate-app/public/login.js
+++ Roomate-app/public/login.js
@@ -1,78 +1,133 @@
 ﻿function login() {
-    localStorage.clear();
-    sessionStorage.clear();
-    let token, user, name, email;
-    var provider = new firebase.auth.GoogleAuthProvider();
-    let db = firebase.firestore();
-    firebase
-      .auth()
-      .signInWithPopup(provider)
-      .then(function(result) {
-        token = result.credential.accessToken;
-        user = result.user;
-        redirLogin(user, db);
-      })
-      .catch(function(error) {
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        console.log("ERROR: " + errorCode + " : " + errorMessage);
-      });
-  }
-  
-  function logout() {
-    localStorage.clear();
-    sessionStorage.clear();
-    window.location.href = './index.html';
-  }
-  
-  function redirLogin(user, database){
-    let mateRef = database.collection("Mates");
-    sessionStorage.setItem(0, user.email);
-    let mate = mateRef
-      .where("usrEmail", "==", user.email)
-      .get()
-      .then(snapshot => {
-        if (snapshot.empty) {
-          sessionStorage.setItem(1, user.displayName);
-          sessionStorage.setItem(2, user.photoURL);
-          window.location.href = './welcome.html';
-          return;
-        }
-        window.location.href = './overview.html';
-      }).catch(err => {
-          console.log('Error getting documents', err);
-      });
-  }
-
-  function loginNewUser(redir){
-    sessionStorage.removeItem(1);
-    let database = firebase.firestore();
-    database.collection("Mates").add({
-        usrName: document.getElementById("userName").value,
-        usrNickname:  document.getElementById("nickname").value,
-        usrEmail: sessionStorage.getItem(0),
-        usrPhotoUrl: sessionStorage.getItem(2)
-    }).then(ref=>{
-        sessionStorage.removeItem(2);
-    }).then(ref =>{
-        window.location.href = redir;
+  localStorage.clear();
+  sessionStorage.clear();
+  let token, user;
+  var provider = new firebase.auth.GoogleAuthProvider();
+  let db = firebase.firestore();
+  let date = new Date();
+  date.setTime(date.getTime() + 86400000);
+  firebase
+    .auth()
+    .signInWithPopup(provider)
+    .then(function(result) {
+      token = result.credential.accessToken;
+      user = result.user;
+      sessionStorage.setItem(0, token);
+      sessionStorage.setItem(1, user.email);
+      redirLogin(user, date, db);
+    })
+    .catch(function(error) {
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      console.log("ERROR: " + errorCode + " : " + errorMessage);
     });
-  }
-  function initializeWelcome(){
-      if(!sessionStorage.getItem(0).includes('@')){
-          window.location.href = "./index.html";
-      }
-      initialize();
-      database = firebase.firestore();
-      let query = database.collection('Mates').where('usrEmail', '==', sessionStorage.getItem(0)).get().then(snapshot =>{
-          if(!snapshot.empty){
-              window.location.href = "./index.html";
-          }else{
-              return;
-          }
-      });
-      document.getElementById("userName").value = sessionStorage.getItem(1);
-      document.getElementById("nickname").value = sessionStorage.getItem(1);
-  }
+}
 
- 
+function logout() {
+  let db = firebase.firestore();
+  db.collection("Mates")
+    .where("usrToken", "==", sessionStorage.getItem(0))
+    .get()
+    .then(snapshot => {
+      snapshot.forEach(doc => {
+        db.collection("Mates")
+          .doc(doc.id)
+          .update({
+            usrToken: "",
+            usrExpiration: new Date()
+          });
+      });
+    });
+    localStorage.clear();
+  sessionStorage.clear();
+  window.location.href = './index.html';
+}
+
+function redirLogin(user, authExpiration, database) {
+  let mateRef = database.collection("Mates");
+  let mate = mateRef
+    .where("usrEmail", "==", user.email)
+    .get()
+    .then(snapshot => {
+      if (snapshot.empty) {
+        sessionStorage.setItem(2, user.displayName);
+        mateRef
+          .add({
+            usrToken: sessionStorage.getItem(0),
+            usrExpiration: authExpiration,
+            usrPhotoUrl: user.photoURL,
+            usrEmail: user.email,
+            usrName: user.displayName,
+            usrNickname: user.displayName
+          })
+          .then(ref => {
+            window.location.href = "./welcome.html";
+          });
+      }
+      snapshot.forEach(ref => {
+        //   console.log(ref.id);
+        //   console.log(sessionStorage.getItem(0));
+        $.when(mateRef.doc(ref.id).update({
+            usrToken: sessionStorage.getItem(0),
+            usrExpiration: authExpiration
+        }))
+        .then(ref => {
+            //window.location.href = './overview.html';
+        });
+      });
+    });
+}
+
+function loginNewUser(redir) {
+  sessionStorage.removeItem(2);
+  let database = firebase.firestore();
+  console.log(sessionStorage.getItem(0));
+  database
+    .collection("Mates")
+    .where("usrToken", "==", sessionStorage.getItem(0))
+    .get()
+    .then(snapshot => {
+      snapshot.forEach(ref => {
+        database
+        .collection("Mates")
+        .doc(ref.id).set({
+          usrName: document.getElementById("userName").value,
+          usrNickname: document.getElementById("nickname").value,
+      }, {merge:true});
+      sessionStorage.setItem(1, ref.id);
+    });
+  
+    })
+    .then(ref => {
+      sessionStorage.removeItem(3);
+    })
+    .then(ref => {
+      window.location.href = redir;
+    });
+}
+function initializeWelcome() {
+  initialize();
+  database = firebase.firestore();
+  let query = database
+    .collection("Mates")
+    .where("usrToken", "==", sessionStorage.getItem(0));
+  query
+    .get()
+    .then(snapshot => {
+      if (snapshot.empty) {
+        window.location.href = "./index.html";
+      } else {
+        snapshot.forEach(doc => {
+          if (new Date() < doc.data().usrExpiration.toDate()) {
+            sessionStorage.setItem(4, doc.id);
+          } else {
+            window.location.href = "./index.html";
+          }
+        });
+      }
+    })
+    .then(ref => {
+      document.getElementById("userName").value = sessionStorage.getItem(2);
+      document.getElementById("nickname").value = sessionStorage.getItem(2);
+    });
+}
