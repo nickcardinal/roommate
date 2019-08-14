add5d5f74665325d3fcb6063685050455f344de1
Andre Tran <atran112@ucr.edu>
2019-08-14 21:09:03 +0100

    Edited the CSS file so it will work on form. Edited the Mate.js file: removed google Auth token variable and deleted those functions, changed function name from createfirestoremate to addNewMate, added functionality to addNewMate function to update the database rather than write to it  (we will decide on how we will write to the database later), and created an assignMateInfo function so that we will be able to display their information back to them. Last, added css to the profile.html to fit with the theme.

--- Roomate-app/public/Mate.js
+++ Roomate-app/public/Mate.js
@@ -2,22 +2,22 @@
 class Mate {
     
     constructor() {
-        this.googleAuthToken;
+        //this.googleAuthToken;
         this.user_ID;
         this.nickName;
         this.fullName;
         this.email;
-        this.photoUrl;
+        this.photoURL;
         //test commit
     }
     
-    setToken(googleAuthToken) {
-        this.googleAuthToken = googleAuthToken;
-    }
-    
-    getToken() {
-        return this.googleAuthToken;
-    }
+//    setToken(googleAuthToken) {
+//        this.googleAuthToken = googleAuthToken;
+//    }
+//
+//    getToken() {
+//        return this.googleAuthToken;
+//    }
     
     setID(user_ID) {
         this.user_ID = user_ID;
@@ -60,22 +60,29 @@ class Mate {
     }
 }
 
-function createFirestoreMate() {
+function addNewMate() {
     var matedb = firebase.firestore();
     
-    var currMate = new Mate();
-    
-    currMate.fullName = "John Doe";
-    currMate.email = "jDoe@ucr.edu";
-    currMate.photoUrl = "jDoe.com";
-    
-    //console.log(Andre.getFullName());
-    
-    matedb.collection("Mates").add(
-       {
-       usrNickName: $("#nameField").val(),
-       usrFullName: currMate.fullName,
-       usrEmail: currMate.email,
-       usrPhotoURL: currMate.photoUrl,
-       });
+    var mate = db.collection('Mates').doc(sessionStorage.getItem(1));
+    mate.update({
+        userNickname: document.getElementById($("#nameField").val()),
+    })
+    //usrNickName: $("#nameField").val(),
 }
+
+function assignMateInfo(docId, db) {
+    let mate = db.collection('Mates').doc(docId);
+    
+    this.user_ID = docId;
+    this.fullName = mate.data().usrName;
+    this.nickName = mate.data().usrNickName;
+    this.email = mate.data().usrEmail;
+    this.photoURL = mate.data().usrPhotoUrl;
+}
+
+
+//this.user_ID;
+//this.nickName;
+//this.fullName;
+//this.email;
+//this.photoURL;
