727a5c226d374affe1dff0c4d0b15fddf1da4886
Mina <dlscarmina@gmail.com>
2019-08-14 17:27:42 +0100

    Modified Task to have needed variables

--- Roomate-app/public/Task.js
+++ Roomate-app/public/Task.js
@@ -1,11 +1,10 @@
 ﻿class Task {
     constructor(taskId, spaceId) {
-        this.taskCategories = new Array();
-        this.title = '';
-        this.time = new Date();
-        this.complete = false;
-        this.desc = '';
-        this.assignedMembers = new Array();
-        this.sid;
+	this.name;
+	this.description;
+	this.dueDate;
+	this.dueTime;
+	this.assignedMate;
+	this.completionStatus;
     }
 }
