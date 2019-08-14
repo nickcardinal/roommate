216000108c4d6276f40c3248b138a89c58d7db34
meidam <53287473+meidam@users.noreply.github.com>
2019-08-14 13:01:57 -0700

    Created a function to create new spaces and and currently working on a function to access spaces from firestore (#15)

--- Roomate-app/public/spaceKey.js
+++ Roomate-app/public/spaceKey.js
@@ -1,8 +1,10 @@
-function spaceKeyInitialize() {
-  var space = new Space();
-  space.setID(9876543);
-  space.setName("My Space");
 
-  document.getElementById('showName').innerHTML= space.getName();
-  document.getElementById('showID').innerHTML= space.getID();
+function spaceKeyInitialize() {
+  initialize();
+  //let spc = new Space();
+  let spc = accessFirestoreSpace('pucXA6FNUDQE2FLCMsic');;
+  spc.setTitle("My Space");
+  spc.setID(1234456);
+  document.getElementById('showName').innerHTML= spc.getTitle();
+  document.getElementById('showID').innerHTML= spc.getID();
 }
