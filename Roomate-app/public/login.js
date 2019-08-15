8248f1adf3507624cd4e30003787afc79eea3f52
jstut001 <46539483+jstut001@users.noreply.github.com>
2019-08-15 08:46:43 +0100

    Added setPersistence to login

--- Roomate-app/public/login.js
+++ Roomate-app/public/login.js
@@ -6,21 +6,22 @@
   let db = firebase.firestore();
   let date = new Date();
   date.setTime(date.getTime() + 86400000);
-  firebase
-    .auth()
-    .signInWithPopup(provider)
-    .then(function(result) {
-      token = result.credential.accessToken;
-      user = result.user;
-      sessionStorage.setItem('token', token);
-      sessionStorage.setItem('email', user.email);
-      redirLogin(user, date, db);
-    })
-    .catch(function(error) {
-      var errorCode = error.code;
-      var errorMessage = error.message;
-      console.log("ERROR: " + errorCode + " : " + errorMessage);
-    });
+  firebase.auth().setPersistence(firebase.auth.Auth.Persistence.LOCAL).then(function() {
+    firebase
+      .auth()
+      .signInWithPopup(provider)
+      .then(function(result) {
+        token = result.credential.accessToken;
+        user = result.user;
+        sessionStorage.setItem('token', token);
+        sessionStorage.setItem('email', user.email);
+        redirLogin(user, date, db);
+      });
+  }).catch(function(error) {
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    console.log("ERROR: " + errorCode + " : " + errorMessage);
+  });
 }
 
 function logout() {
