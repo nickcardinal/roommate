920deec70f7ebb9bcb2c171e936a2db80795b979
Mina <dlscarmina@gmail.com>
2019-08-15 10:28:57 +0100

    Updated data collection funciton to use getters/setters

--- Roomate-app/public/Task.js
+++ Roomate-app/public/Task.js
@@ -1,109 +1,109 @@
 ﻿class Task {
-	constructor(taskId, spaceId) {
-		// Task Descriptors
-		this.title;
-		this.description;
+  constructor(taskId, spaceId) {
+  	// Task Descriptors
+  	this.title;
+  	this.description;
 
-		// Task Deadline Data
-		this.dueYear;
-		this.dueDay;
-		this.dueMonth;
-		this.dueTime; // 0000 to 2359
+  	// Task Deadline Data
+  	this.dueYear;
+  	this.dueDay;
+  	this.dueMonth;
+  	this.dueTime; // 0000 to 2359
 
-		// Task Completion Details
-		this.assignedMate;
-		this.completionStatus;
-	}
+  	// Task Completion Details
+  	this.assignedMate;
+  	this.completionStatus;
+  }
 
-	// Getters/Setters: Task Descriptors
-	setTitle(title) {
-		this.title = title;
-	}
+  // Getters/Setters: Task Descriptors
+  setTitle(title) {
+  	this.title = title;
+  }
 
-	getTitle() {
-		return this.title;
-	}
+  getTitle() {
+  	return this.title;
+  }
 
-	setDescription(description) {
-		this.description = description;
-	}
+  setDescription(description) {
+  	this.description = description;
+  }
 
-	getDescription(description) {
-		return this.description;
-	}
+  getDescription(description) {
+  	return this.description;
+  }
 
-	// Getters/Setters: Task Deadline Data
-	setDueYear(dueYear) {
-		this.dueYear = dueYear;
-	}
+  // Getters/Setters: Task Deadline Data
+  setDueYear(dueYear) {
+  	this.dueYear = dueYear;
+  }
 
-	getDueYear(dueYear) {
-		return this.dueYear;
-	}
+  getDueYear(dueYear) {
+  	return this.dueYear;
+  }
 
-	setDueMonth(dueMonth) {
-		this.dueMonth = dueMonth;
-	}
+  setDueMonth(dueMonth) {
+  	this.dueMonth = dueMonth;
+  }
 
-	getDueMonth(dueMonth) {
-		return this.dueMonth;
-	}
+  getDueMonth(dueMonth) {
+  	return this.dueMonth;
+  }
 
-	setDueDay(dueDay) {
-		this.dueDay = dueDay;
-	}
+  setDueDay(dueDay) {
+  	this.dueDay = dueDay;
+  }
 
-	getDueDay(dueDateDay) {
-		return this.dueDay;
-	}
+  getDueDay(dueDateDay) {
+  	return this.dueDay;
+  }
 
-	setDueTime(dueTime) {
-		this.dueTime = dueTime;
-	}
+  setDueTime(dueTime) {
+  	this.dueTime = dueTime;
+  }
 
-	getDueTime(dueTime) {
-		return this.dueTime;
-	}
+  getDueTime(dueTime) {
+  	return this.dueTime;
+  }
 
-	// Getters/Setters: Task Completion Details
-	setAssignedMate(assignedMate) {
-		this.assignedMate = assignedMate;
-	}
+  // Getters/Setters: Task Completion Details
+  setAssignedMate(assignedMate) {
+  	this.assignedMate = assignedMate;
+  }
 
-	getAssignedMate(assignedMate) {
-		return this.assignedMate;
-	}
+  getAssignedMate(assignedMate) {
+  	return this.assignedMate;
+  }
 
-	setCompletionStatus(completionStatus) {
-		this.completionStatus = completionStatus;
-	}
+  setCompletionStatus(completionStatus) {
+  	this.completionStatus = completionStatus;
+  }
 
-	getCompletionStatus(completionStatus) {
-		return this.completionStatus;
-	}
+  getCompletionStatus(completionStatus) {
+  	return this.completionStatus;
+  }
+
 }
 
 //Create Firestore Task
 function createFirestoreTask() {
-	var taskdb = firebase.firestore();
-	var currTask = new Task();
+	var taskdb = firebase.firestore().collection("Task");
 
-	// currTask.title = "Take out the trash";
-	// currTask.description = "Remember to replace the bag!";
-	// currTask.dueYear = 0;
-	// currTask.dueMonth = 0;
-	// currTask.dueDay = 0;
-	// currTask.dueTime = 0;
-	currTask.assignedMate = "Unique Mate ID"
-	currTask.completionStatus = false;
+  let currTask = new Task();
+  currTask.setTitle($("#titleField").val());
+  currTask.setDescription($("#descriptionField").val());
+  currTask.setDueYear($("#dueYearField").val());
+  currTask.setDueMonth($("#dueMonthField").val());
+  currTask.setDueDay($("#dueDayField").val());
+  currTask.setDueTime($("#dueTimeField").val());
 
-	taskdb.collection("Task").add({
-		tskTitle: $("#titleField").val(),
-		tskDescription: $("#descriptionField").val(),
+  let data = {
+    tskTitle: currTask.getTitle(),
+    tskDescription: currTask.getDescription(),
+    tskDueYear: currTask.getDueYear(),
+    tskDueMonth: currTask.getDueMonth(),
+    tskDueDay: currTask.getDueDay(),
+    tskDueTime: currTask.getDueTime(),
+  }
 
-		tskDueYear: $("#dueYearField").val(),
-		tskDueMonth: $("#dueMonthField").val(),
-		tskDueDay: $("#dueDayField").val(),
-		tskDueTime: $("#dueTimeField").val(),
-	});
+	taskdb.add(data);
 }
