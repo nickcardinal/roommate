aa615b82d08124ded8bc19317206e0dca591f178
atran112 <53277408+atran112@users.noreply.github.com>
2019-08-16 15:34:06 +0100

    Atran112/function mate least tasks (#28)
    * Starting on the number of assignments function, but have to stop because I just realized there is an error in my other branch
    
    * Created function that will return the number of tasks a mate is assigned to.
    
    * Uploaded an empty Space test file.
    
    * Created a function that will return the nickName of the Mate with the least amount of task assigned. It iterates through all mates nickname's and calls the function mateNumTask. This is a branch based off of the atran112/assignment-recur-function branch, so it does NOT branch from master.
    
    * Updated mateNumTask to use email rather than nickname
    
    * Fixed minNumTasks to call minNumTasks with email instead of nickname, and randomly choose from a list of all mates with the least amount of tasks
    
    * Tried to add some tests for jest, can't get space to inititialize
    
    * Fixed naming convention
    
    * Fixed naming
    
    * Changed from the implicit variable email to the getter function.
    
    * Update Space.js
    
    * Update Space.js

--- Roomate-app/public/scripts/Space.js
+++ Roomate-app/public/scripts/Space.js
@@ -1,95 +1,96 @@
 class Space {
-    constructor() {
-        this.title;
-        this.description;
-        this.ID;
-        this.mates = [];
-        this.tasks = [];
-    }
+  constructor() {
+    this.title;
+    this.description;
+    this.ID;
+    this.mates = [];
+    this.tasks = [];
+  }
 
-    //Title Functions
-    setTitle(title) {
-      this.title = title;
-    }
+  //Title Functions
+  setTitle(title) {
+    this.title = title;
+  }
 
-    getTitle() {
-      return this.title;
-    }
+  getTitle() {
+    return this.title;
+  }
 
-    //Description Functions
-    setDescription(description) {
-      this.description = description;
-    }
+  //Description Functions
+  setDescription(description) {
+    this.description = description;
+  }
 
-    getDescription() {
-      return this.description;
-    }
+  getDescription() {
+    return this.description;
+  }
 
-    //ID Functions
-    setID(ID) {
-      this.ID = ID;
-    }
+  //ID Functions
+  setID(ID) {
+    this.ID = ID;
+  }
 
-    getID() {
-      return this.ID;
-    }
+  getID() {
+    return this.ID;
+  }
 
-    //Mate Array Functions
-    addMate(mate) {
-      this.mates.push(mate);
-    }
+  //Mate Array Functions
+  addMate(mate) {
+    this.mates.push(mate);
+  }
 
-    getMates() {
-      return this.mates;
-    }
+  getMates() {
+    return this.mates;
+  }
 
-    //Task Array Functions
-    addTask(task) {
-      this.tasks.push(task);
-    }
+  //Task Array Functions
+  addTask(task) {
+    this.tasks.push(task);
+  }
 
-    getTasks() {
-      return this.tasks;
-    }
+  getTasks() {
+    return this.tasks;
+  }
 
-	addMateToSpace(userDocID){
-		var db = firebase.firestore();
-		var spcDocRef = db.collection("Spaces").doc(this.ID);
+  addMateToSpace(userDocID) {
+    var db = firebase.firestore();
+    var spcDocRef = db.collection("Spaces").doc(this.ID);
 
-		db.runTransaction(transaction => {
-		  return transaction.get(spcDocRef).then(snapshot => {
-			const spcUserArray = snapshot.get('spcMates');
-			spcUserArray.push(userDocID);
-			transaction.update(spcDocRef, 'spcMates', spcUserArray);
-		  });
-		});
-	}
+    db.runTransaction(transaction => {
+      return transaction.get(spcDocRef).then(snapshot => {
+        const spcUserArray = snapshot.get("spcMates");
+        spcUserArray.push(userDocID);
+        transaction.update(spcDocRef, "spcMates", spcUserArray);
+      });
+    });
+  }
 
-	isValidSpace(spaceDocID, _callback){
-		var db = firebase.firestore();
-		var spcDocRef = db.collection('Spaces').doc(spaceDocID);
+  isValidSpace(spaceDocID, _callback) {
+    var db = firebase.firestore();
+    var spcDocRef = db.collection("Spaces").doc(spaceDocID);
 
-		var exists = false;
-		var test = spcDocRef.get()
-							.then(function(doc) {
-								if (doc.exists) {
-									exists =  true;
-								} else {
-									exists =  false;
-								}
-							})
-							.catch(function(error) {
-								console.log("Error getting document:", error);
-								exists =  false;
-							});
+    var exists = false;
+    var test = spcDocRef
+      .get()
+      .then(function(doc) {
+        if (doc.exists) {
+          exists = true;
+        } else {
+          exists = false;
+        }
+      })
+      .catch(function(error) {
+        console.log("Error getting document:", error);
+        exists = false;
+      });
 
-		return _callback(exists);
-	}
+    return _callback(exists);
+  }
 
-	outputMatesInSpace(){
-		var db = firebase.firestore();
-		var spcDocRef = db.collection('Spaces').doc('sFSKvtwdCrpXCMGsdkHP');
-	}
+  outputMatesInSpace() {
+    var db = firebase.firestore();
+    var spcDocRef = db.collection("Spaces").doc("sFSKvtwdCrpXCMGsdkHP");
+  }
 
   getNumberOfTasksByMateEmail(email) {
     var numTasks = 0;
@@ -99,9 +100,35 @@ class Space {
       }
     }
     return numTasks;
-    
-  getNextMateAssignedToRecurringTask(email) {
+  }
 
+  getMateToAssignToTask() {
+    if (this.mates.length == 0) {
+      console.log("no mates in the living space");
+      return; //condition here just in case
+    }
+
+    let minNumTasks = this.getNumberOfTasksByMateEmail(
+      this.mates[0].getEmail()
+    );
+    var minTaskMates = new Mate[this.mates[0]]();
+
+    for (var i = 1; i < this.mates.length; ++i) {
+      let j = this.getNumberOfTasksByMateEmail(this.mates[i].getEmail()); //would be more efficient to get all the number of tasks in one shot...
+      if (j < minNumTasks) {
+        minNumTasks = j;
+        minTaskMates = new Mate[this.mates[i]]();
+      } else if (j === minNumTasks) {
+        minTaskMates.push(this.mates[i]);
+      }
+    }
+    if (minTaskMates.length > 1) {
+      return minTaskMates[Math.random() * minTaskMates.length];
+    }
+    return minTaskMates[0];
+  }
+
+  getNextMateAssignedToRecurringTask(email) {
     for (var i = 0; i < this.mates.length - 1; ++i) {
       if (this.mates[i].getEmail() == email) {
         return this.mates[i + 1].getEmail();
@@ -109,50 +136,55 @@ class Space {
     }
     return this.mates[0].getEmail();
   }
-
 }
-
-function testSpace(){
-	var newSpace = new Space();
-	console.log(newSpace.isValidSpace("sFSKvtwdCrpXCMGsdkHP", outputFunction));
+function testSpace() {
+  var newSpace = new Space();
+  console.log(newSpace.isValidSpace("sFSKvtwdCrpXCMGsdkHP", outputFunction));
 }
-function outputFunction(exists){
-	return exists;
+function outputFunction(exists) {
+  return exists;
 }
 function createFirestoreSpace() {
-    let spacedb = firebase.firestore().collection("Spaces");
+  let spacedb = firebase.firestore().collection("Spaces");
 
-    //testSpace
-    let currSpace = new Space();
-    currSpace.setTitle("My Space");
-    currSpace.setDescription("This is my new space");
+  //testSpace
+  let currSpace = new Space();
+  currSpace.setTitle("My Space");
+  currSpace.setDescription("This is my new space");
 
-    let data = {
-      spcTitle: currSpace.getTitle(),
-      spcDescription: currSpace.getDescription(),
-      spcMates: firebase.firestore.FieldValue.arrayUnion('/Mates/mnTsbYn8LSlug7JlYsxW')
-    }
+  let data = {
+    spcTitle: currSpace.getTitle(),
+    spcDescription: currSpace.getDescription(),
+    spcMates: firebase.firestore.FieldValue.arrayUnion(
+      "/Mates/mnTsbYn8LSlug7JlYsxW"
+    )
+  };
 
-    spacedb.add(data);
+  spacedb.add(data);
 }
 
 function accessFirestoreSpace(ID, _callback) {
   let space = new Space();
-  let spacedb = firebase.firestore().collection("Spaces").doc(ID);
-  let getSpace = spacedb.get().then(doc => {
-    if (!doc.exists) {
-      console.log('No such document!');
-    } else {
-      space.setDescription(doc.data().spcDescription);
-      space.setTitle(doc.data().spcTitle);
-      space.setID(doc.id);
+  let spacedb = firebase
+    .firestore()
+    .collection("Spaces")
+    .doc(ID);
+  let getSpace = spacedb
+    .get()
+    .then(doc => {
+      if (!doc.exists) {
+        console.log("No such document!");
+      } else {
+        space.setDescription(doc.data().spcDescription);
+        space.setTitle(doc.data().spcTitle);
+        space.setID(doc.id);
 
-      _callback(space);
-    }
-  })
-  .catch(err => {
-    console.log('Error getting document', err);
-  });
+        _callback(space);
+      }
+    })
+    .catch(err => {
+      console.log("Error getting document", err);
+    });
 }
 
 function reWriteFirestoreSpace(ID, space) {
@@ -162,7 +194,7 @@ function reWriteFirestoreSpace(ID, space) {
   let data = {
     spcTitle: space.getTitle(),
     spcDescription: space.getDescription()
-  }
+  };
 
   spacedb.doc(ID).set(data);
 }
