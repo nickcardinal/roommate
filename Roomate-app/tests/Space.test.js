6eae4f01697fdb210a1dfe924b6d9a1faba1bd35
atran112 <53277408+atran112@users.noreply.github.com>
2019-08-18 17:04:42 +0100

    Created a test for spaces. (#49)

--- Roomate-app/tests/Space.test.js
+++ Roomate-app/tests/Space.test.js
@@ -2,7 +2,7 @@ const Space = require("../public/scripts/Space.js");
 const Mate = require("../public/scripts/Mate.js");
 //create some data;
 
-let spaces = new Space[new Space(), Space(), Space(), Space(), Space()];//length = 5
+let testSpace = new Space();
 
 /*
 test('some description', function(){
@@ -10,51 +10,50 @@ test('some description', function(){
 });
 */
 
-for(let i = 0; i < 2; i++){
-    spaces[i].setTitle('Space ' + i);
-    spaces[i].setDescription('Space description for ' + i);
-    spaces[i].setID('IDFORSPACE' + i);
-    for(let j = 0; j < 5; j++){
-        let mate = new Mate();
-        mate.setID('MATEID' + j + 'SPACEID' + i);
-        mate.setNickName('m' + j);
-        mate.setFullName('Mate ' + j);
-        mate.setEmail('mate' + j + 's' + i + '@test.com');
-        mate.setPhotoURL('mate' + j +'ulr' + i);
-        spaces[i].addMate(mate);
-    }
+testSpace.setTitle("Andre's Space");
+testSpace.setDescription("The Collective");
+testSpace.setID("88RYZING");
+
+for(let j = 0; j < 5; j++){
+    let mate = new Mate();
+    mate.setID('MATEID' + j + 'SPACEID');
+    mate.setNickName('m' + j);
+    mate.setFullName('Mate ' + j);
+    mate.setEmail('mate' + j + '@test.com');
+    mate.setPhotoURL('mate' + j +'ulr');
+    testSpace.addMate(mate);
 }
 
 console.log('Initialized');
 
 test('Set Title set proper title for space 1', function(){
-    expect(spaces[1].getTitle()).toEqual('Space 1');
+    expect(testSpace.getTitle()).toEqual("Andre's Space");
 });
 
 test('Set Description set proper description for space 1', function(){
-    expect(spaces[1].getDescription()).toEqual('Space description for 1');
+    expect(testSpace.getDescription()).toEqual("The Collective");
 });
 
 test('Set ID set proper ID for space 1', function(){
-    expect(spaces[1].getID()).toEqual('IDFORSPACE1');
+    expect(testSpace.getID()).toEqual('88RYZING');
 });
 
 test('add Mate set proper mate 3 for space 1', function(){
-    expect(spaces[1].getMates()[3].getEmail).toEqual('mate3s1@test.com');
+    expect(testSpace.getMates()[3].getEmail()).toEqual('mate3@test.com');
 });
 
-test('Set Title set proper title for space 2', function(){
-    expect(spaces[2].getTitle()).toEqual('Space 2');
-});
+// test('Set Title set proper title for space 2', function(){
+//     expect(testSpace.getTitle()).toEqual('Space 2');
+// });
 
-test('Set Description set proper description for space 1', function(){
-    expect(spaces[1].getDescription()).toEqual('Space description for 1');
-});
+// test('Set Description set proper description for space 1', function(){
+//     expect(testSpace.getDescription()).toEqual('Space description for 1');
+// });
 
-test('Set ID set proper ID for space 2', function(){
-    expect(spaces[2].getID()).toEqual('IDFORSPACE2');
-});
+// test('Set ID set proper ID for space 2', function(){
+//     expect(testSpace.getID()).toEqual('IDFORSPACE2');
+// });
 
-test('add Mate set proper mate 3 for space 2', function(){
-    expect(spaces[2].getMates()[3].getEmail).toEqual('mate3s2@test.com');
-});
+// test('add Mate set proper mate 3 for space 2', function(){
+//     expect(testSpace.getMates()[3].getEmail).toEqual('mate3s2@test.com');
+// });
