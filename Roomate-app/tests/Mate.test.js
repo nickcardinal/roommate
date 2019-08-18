ab5289d27a83531925f9e3737b57d5f22212f817
nickcardinal <43731170+nickcardinal@users.noreply.github.com>
2019-08-18 12:22:22 +0100

    Moved .test files from public to tests folder (#41)
    * Moved .test files from public to tests folder
    
    * Removed some debugging code from config.js

---
+++ Roomate-app/tests/Mate.test.js
@@ -0,0 +1,19 @@
+const user = require("../public/scripts/Mate.js")
+
+test('Getter/Setter Test', function({
+	var user_ID = 'gjh43rt43tghrj';
+	var nickName = 'Tommy Salami';
+	var fullName = 'Alan Turing';
+	var email = 'alanturntup@oal.com';
+	var photoURL = 'https://google.com/images/theBestImage.png';
+	
+	var mate = new Mate();
+	mate.setID(user_ID);
+	mate.setNickName(nickName);
+	mate.setFullName(fullName);
+	mate.setEmail(email);
+	mate.setPhotoURL(photoURL);
+	
+	expect([mate.getID(), mate.getNickName(), mate.getFullName(), mate.getEmail(), mate.getPhotoURL()])
+											  .toEqual([user_ID, nickName, fullName, email, photoURL]);
+});
