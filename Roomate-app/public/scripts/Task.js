4637102e5d588a302e8815f3f04b7ddb09c8e4d5
jstut001 <46539483+jstut001@users.noreply.github.com>
2019-08-18 14:13:36 +0100

    Jesse - Fixed naming issues with RandomAssign fctns (#46)
    * switched data calls to function calls
    
    * Debugged random assign for task assignment.

--- Roomate-app/public/scripts/Task.js
+++ Roomate-app/public/scripts/Task.js
@@ -108,7 +108,7 @@ function createFirestoreTask() {
 	taskdb
 		.add(data)
 		.then(function(docRef) {
-			console.log("Space in session: " + docRef);
+			console.log("Space in session: " + docRef.id);
 
 			//Add Task to Space
 			var spaceID = sessionStorage.getItem("Spaces");
