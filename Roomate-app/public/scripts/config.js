e0a145cf3d1550787981d107cae172fff6f0343d
nickcardinal <ncard005@ucr.edu>
2019-08-18 09:25:34 +0100

    Removed some debugging code from config.js

--- Roomate-app/public/scripts/config.js
+++ Roomate-app/public/scripts/config.js
@@ -12,7 +12,6 @@ const firebaseConfig = {
 };
 function validate(){
     firebase.initializeApp(firebaseConfig);
-	return;
     database = firebase.firestore();
     if(sessionStorage.getItem('log') === 'true'){
       updateToken_Overview(database);  
