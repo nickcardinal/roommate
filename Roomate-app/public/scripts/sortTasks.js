635462a7e832337f8f70851e4ca8f74267c4eb93
Mina <dlscarmina@gmail.com>
2019-08-20 14:01:33 +0100

    Trying to get task array locally to prep for sorting

---
+++ Roomate-app/public/scripts/sortTasks.js
@@ -0,0 +1,12 @@
+var spaceID = sessionStorage.getItem("Space");
+var spacedb = firebase.firestore().collection("Spaces").doc(spaceID);
+console.log("Space in session: " + spaceID);
+
+var tempTaskArray = new Array();
+
+let space = await spacedb.get();
+tempTaskArray = space.data().spcTasks;
+console.log("tempTaskArray: ");
+tempTaskArray.forEach(doc => {
+  console.log(doc);
+})
