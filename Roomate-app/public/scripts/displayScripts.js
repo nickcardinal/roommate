9e01ead7294afeda63e9b0729637d30b2592c22d
jstut001 <46539483+jstut001@users.noreply.github.com>
2019-08-20 15:48:53 +0100

    Made dynamic table creation

--- Roomate-app/public/scripts/displayScripts.js
+++ Roomate-app/public/scripts/displayScripts.js
@@ -1,3 +1,28 @@
-function displayTasks(){
+function displayTasks(tasks) {
+    tasks.forEach(task => {
+        appendTask(task);
+    })
+}
 
+function appendTask(task) {
+  let table = document.getElementById("taskList");
+  let rows = table.getElementsByTagName("tr");
+  let row = table.insertRow(rows.length);
+  let tskTitle = row.insertCell(0);
+  let tskComplete = row.insertCell(1);
+  row = table.insertRow(rows.length);
+  let tskDesc = row.insertCell(0);
+  row = table.insertRow(rows.length);
+  let tskDue = row.insertCell(0);
+  row = table.insertRow(rows.length);
+  let br = row.insertCell(0);
+  tskTitle.innerHTML = task.getTitle();
+  tskComplete.innerHTML = '<button onclick=completeTask(' + task.getTaskID() + ')>Mark Complete</button>';
+  tskDesc.innerHTML = task.getDescription();
+  tskDue.innerHTML = 'Due by ' + task.getDueDate() + ' ' + task.getDueTime();
+  br.innerHTML = '<br></br>'
+}
+
+function completeTask(taskID){
+    //marks task with id taskID as completed
 }
