7310ffaf9ccd0868ef362b28b6b7c63ebe4d42f0
jstut001 <46539483+jstut001@users.noreply.github.com>
2019-08-20 13:28:43 +0100

    Update sort.js

--- Sandbox/random tests/sort.js
+++ Sandbox/random tests/sort.js
@@ -1,22 +1,45 @@
+function print(list){
+    list.forEach(item => {
+        console.log('a:',item.a, '\tb:', item.b, '\tc:', item.c);
+    });
+}
+
 let list = new Array();
 let i = 0;
 for(i = 0; i < 100; i++){
     list.push({a:Math.floor(Math.random() * 10), b:Math.floor(Math.random() * 10), c:Math.floor(Math.random() * 10)})
 }
-list.sort((a, b) => {
-    if(a.a > b.a){
+console.log('\n------------Starting Data------------\n');
+print(list);
+list.sort(function(A, B){
+    if(A.a > B.a){
         return 1;
-    }else if(a.a === b.a){
-        if(a.b > b.b){
+    }else if(A.a === B.a){
+        if(A.b > B.b){
             return 1;
-        }else if(a.b === b.b){
-            if(a.c >= b.c){
+        }else if(A.b === B.b){
+            if(A.c >= B.c){
                 return 1;
            }
         }
     }
     return -1;
 });
-list.forEach(item => {
-    console.log('a:',item.a, '\tb:', item.b, '\tc:', item.c);
+console.log('\n------------Sorted Data------------\n');
+print(list);
+return;
+
+list.sort(function(a, b){
+    if(a.getDueDate() > b.getDueDate()){
+        return 1;
+    }else if(a.getDueDate() === b.getDueDate()){
+        if(a.getDueTime() > b.getDueTime()){
+            return 1;
+        }else if(a.getDueTime() === b.getDueTime()){
+            if(a.getTitle() > b.getTitle()){
+                return 1;
+            }
+        }
+    }
+    return -1;
 })
