fce799d95cd788d8b677fd54f5e69a0848d2ce1c
Andre Tran <atran112@ucr.edu>
2019-08-20 22:39:01 +0100

    Created multiple different non-recurring task, so that I could actually test. Should have done this from the start, but I was curious what copying a task would do. Also tested this in conjunction with recurring task to make sure that they would be ignored in the nonrecurring algorithm.

--- Roomate-app/tests/Space.test.js
+++ Roomate-app/tests/Space.test.js
@@ -25,22 +25,6 @@ for(let j = 0; j < 5; j++){
     testSpace.addMate(mate);
 }
 
-var task1 = new Task();
-task1.setTitle("Wash Dishes");
-task1.setDescription("Hand Wash Pots and Pans");
-task1.setTaskID("123456");
-task1.setIsRecurring(false);
-task1.setIsComplete(false);
-
-
-task1.setAssignedMate(testSpace.setMateToNonRecurringTask());
-
-testSpace.addTask(task1);
-
-console.log(testSpace.getNumberOfMatesNonRecurringTasks(task1.getAssignedMate()));
-
-console.log(testSpace.tasks[0]);
-
 console.log('Initialized');
 
 test('Set Title set proper title for Space', function(){
@@ -61,8 +45,7 @@ test('add Mate set proper mate 3 for Space', function(){
 
 
 
-//New Tests for recurring Assignment
-
+//Tests for recurring Assignment
 
 var recurTask = new Task();
 recurTask.setTitle("Trash Day");
@@ -79,9 +62,67 @@ recurTask.setAssignedMate(testSpace.setNextMateAssignedToRecurringTask(recurTask
 
 console.log(recurTask);
 
-recurTask.setAssignedMate(testSpace.setNextMateAssignedToRecurringTask(recurTask.getAssignedMate()));
+//placed recurring tasks to make sure that nonrecurring task will ignore them
 
+
+var task1 = new Task();
+task1.setTitle("Wash Dishes");
+task1.setDescription("Hand Wash Pots and Pans");
+task1.setTaskID("123456");
+task1.setIsRecurring(false);
+task1.setIsComplete(false);
+
+var task2 = new Task();
+task2.setTitle("Feed Tiny");
+task2.setDescription("Feed him dog food, not human food lol");
+task2.setTaskID("83893");
+task2.setIsRecurring(false);
+task2.setIsComplete(false);
+
+var task3 = new Task();
+task3.setTitle("Mow Lawn");
+task3.setDescription("Guest are coming");
+task3.setTaskID("09712");
+task3.setIsRecurring(false);
+task3.setIsComplete(false);
+
+
+var task4 = new Task();
+task4.setTitle("Pickup Amazon Package");
+task4.setDescription("It will be dropped off at back door");
+task4.setTaskID("15872");
+task4.setIsRecurring(false);
+task4.setIsComplete(false);
+
+var task5 = new Task();
+task5.setTitle("Pickup Milk");
+task5.setDescription("Lactose free");
+task5.setTaskID("129743");
+task5.setIsRecurring(false);
+task5.setIsComplete(false);
+
+task1.setAssignedMate(testSpace.setMateToNonRecurringTask());
+testSpace.addTask(task1);
+console.log(task1);
+
+task2.setAssignedMate(testSpace.setMateToNonRecurringTask());
+testSpace.addTask(task2);
+console.log(task2);
+
+task3.setAssignedMate(testSpace.setMateToNonRecurringTask());
+testSpace.addTask(task3);
+console.log(task3);
+
+task4.setAssignedMate(testSpace.setMateToNonRecurringTask());
+testSpace.addTask(task4);
+console.log(task4);
+
+recurTask.setAssignedMate(testSpace.setNextMateAssignedToRecurringTask(recurTask.getAssignedMate()));
 console.log(recurTask);
-// test('testing getNextMateAssignedToRecurringTask function', function(){
-//     expect(recurTask.getAssignedMate()).toEqual(nextMate);
-// });
+
+task5.setAssignedMate(testSpace.setMateToNonRecurringTask());
+testSpace.addTask(task5);
+console.log(task5);
+
+
+//break
