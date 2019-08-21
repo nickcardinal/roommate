73054a1907ff6ef6607abf1f9339af146685cf46
Mina <dlscarmina@gmail.com>
2019-08-21 23:53:33 +0100

    Made what was in sortTasks into a funciton

--- Roomate-app/public/scripts/sortTasks.js
+++ Roomate-app/public/scripts/sortTasks.js
@@ -1,12 +1,15 @@
-var spaceID = sessionStorage.getItem("Space");
-var spacedb = firebase.firestore().collection("Spaces").doc(spaceID);
-console.log("Space in session: " + spaceID);
+// An attempt to pull tasks from Firebase
+function pullTasksFromFirebase() {
+  var spaceID = sessionStorage.getItem("Space");
+  var spacedb = firebase.firestore().collection("Spaces").doc(spaceID);
+  console.log("Space in session: " + spaceID);
 
-var tempTaskArray = new Array();
+  var tempTaskArray = new Array();
 
-let space = await spacedb.get();
-tempTaskArray = space.data().spcTasks;
-console.log("tempTaskArray: ");
-tempTaskArray.forEach(doc => {
-  console.log(doc);
-})
+  let space = await spacedb.get();
+  tempTaskArray = space.data().spcTasks;
+  console.log("tempTaskArray: ");
+  tempTaskArray.forEach(doc => {
+    console.log(doc);
+  })
+}
