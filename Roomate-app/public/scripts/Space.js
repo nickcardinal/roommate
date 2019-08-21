5a0367bfa4fc665744a82311c56818916b0cadcb
nickcardinal <43731170+nickcardinal@users.noreply.github.com>
2019-08-21 09:12:58 +0100

    Finished Recurring tasks UI and linked to Firebase db (#64)

--- Roomate-app/public/scripts/Space.js
+++ Roomate-app/public/scripts/Space.js
@@ -73,7 +73,7 @@ class Space {
 						});
     }
 
-    addMateToSpace(userDocID) {
+    addMateToSpace() {
         var userDocID = sessionStorage.getItem("user");
         var userSpaceID = $("#userSpaceID").val();
         this.isValidSpace(userSpaceID).then(function (exists) {
@@ -278,7 +278,10 @@ function outputMatesAndTasksInSpace() {
 		});
     });
 }
-
+function joinExistingSpace(){
+	var newSpace = new Space();
+	newSpace.addMateToSpace();
+}
 function redirectCreateNewSpace() {
     window.location.href = "../html/createNewSpace.html";
 }
