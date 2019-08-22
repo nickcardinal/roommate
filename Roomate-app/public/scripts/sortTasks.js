84240b7ce8b4f4312e533f8137b16698ac5b74b3
Mina <dlscarmina@gmail.com>
2019-08-22 13:01:49 +0100

    Sort works, but need to take into consideration years with more than 4 digits

--- Roomate-app/public/scripts/sortTasks.js
+++ Roomate-app/public/scripts/sortTasks.js
@@ -1,17 +1,46 @@
+var tasksClone;
 
-function sortTasksByDate() {
+function cloneTasksArray() {
   // Pull tasks from Firebase
   var space = new Space();
   space.setID(sessionStorage.getItem('Space'));
+  console.log("Pulled created temp Space object");
+  tasksClone = new Array();
+
+  // Push tasks to Cloned Task List
   var tasks = space.fillTasksArray().then(function(tasksArray) {
+    console.log("Filled temp Space task Array");
     space.tasks = tasksArray;
-    console.log("Task List: ");
     space.tasks.forEach(task => {
-        console.log(task.getDueDate(), task.getDueTime());
+        tasksClone.push(task);
     });
   });
+  outputTaskList(tasksClone);
 }
 
-function moveCompletedTasks() {
+// Sorts Tasks by Date then by Time
+function sortTaskByDate(tasksArray) {
+  tasksArray.sort((taskA, taskB) => {
 
+    // Check Date: recent first
+    if(taskA.getDueDate() > taskB.getDueDate()) {
+      return 1;
+    } else if(taskA.getDueDate() === taskB.getDueDate()){
+
+      // Check Time: recent first
+      if(taskA.getDueTime() >= taskB.getDueTime()) {
+        return 1;
+      }
+    }
+    return -1;
+  });
+
+  outputTaskList(tasksArray);
+}
+
+// Outputs dates and times of Task List to console
+function outputTaskList(tasksArray) {
+  tasksArray.forEach(task => {
+    console.log(task.getDueDate(), task.getDueTime());
+  });
 }
