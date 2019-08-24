d51d224d72032e8d06cb247532a39c85554bb1fa
jstut001 <46539483+jstut001@users.noreply.github.com>
2019-08-24 17:38:02 +0100

    Bug fixed

--- Roomate-app/public/scripts/displayScripts.js
+++ Roomate-app/public/scripts/displayScripts.js
@@ -51,18 +51,9 @@ function appendTask(task, table) {
   }
   tskDesc.innerHTML = task.getDescription();
   tskDue.innerHTML = 'Due by ' + task.getDueDate() + ' ' + task.getDueTime();
-  tskMate.innerHTML = 'Task assigned to ';
+  tskMate.innerHTML = 'Task assigned to ' + getMateByID(task.getAssignedMateID())[0].getNickName();
   if(task.getFavourMate() !== ''){
-    tskMate.innerHTML = 'Task favoured by ';
-    firebase.firestore().collection('Mates').doc(task.getFavourMate()).get().then(doc => {
-      try{tskMate.innerHTML += doc.data().usrNickname;}
-      catch(e){tskMate.innerHTML += '?';}
-    });
-  }else{
-    firebase.firestore().collection('Mates').doc(task.getAssignedMateID()).get().then(doc => {
-      try{tskMate.innerHTML += doc.data().usrNickname;}
-      catch(e){tskMate.innerHTML += '?';}
-    });
+    tskMate.innerHTML = 'Task favoured by ' + getMateByID(task.getFavourMate())[0].getNickName();
   }
   br.innerHTML = '<br></br>'
 }
