27a48278cc328688c105d654bea3ae86f2325e4b
jstut001 <46539483+jstut001@users.noreply.github.com>
2019-08-25 15:49:35 +0100

    Update utility.js

--- Roomate-app/public/scripts/utility.js
+++ Roomate-app/public/scripts/utility.js
@@ -142,19 +142,19 @@ async function completeTask(taskID){
       let fact = new RecurringTaskFactory(firebase.firestore().collection('Tasks'), getMatesInSpace());
       await fact.reCreateTask(task);
       await task.pushComplete();
-      //saveSpaceToSessionStorage();
-      //location.reload();
+      refreshTasks();
     }else{
       await task.pushComplete();
       saveSpaceToSessionStorage();
     }
 }else{
     if(task.getFavorMateID !== ''){//cannot have more than one mate favour a task.
-        //location.reload();
+        refreshTasks();
         return;
     }else{
            await favourTask(task);
            saveSpaceToSessionStorage();
+           refreshTasks();
            return;
          }
 }
